53a0a29e9b7f854a7a2044b1fc2bc71d090039b5
Dan <ohallord@gmail.com>
2018-06-21 21:07:44 -0600

    Set up the deployment

--- cgminer_docker/run_cgminer.sh
+++ cgminer_docker/run_cgminer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-/usr/local/bin/cgminer -o "${POOL_URL}" -u ${MINER_USER}  -p ${MINER_PASSWD} --verbose
+/usr/local/bin/cgminer -o "${POOL_URL}" -u ${MINER_USER}  -p ${MINER_PASSWD}  >>/var/log/cgminer.log 2>&1
